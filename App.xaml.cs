849248257e7ad65c9ce66cba5fa053b21edb240c
David Calvin <dave.calvin@gmail.com>
2025-01-31 17:41:06 -0600

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Llama_IDScanner
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
